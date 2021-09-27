801ed54b99854d0c1e254e013279f49bc3ff3ecd
akashroy1 <royaku6@gmail.com>
2021-09-27 18:00:14 +0530

    Initioal Built only with Navbar and design

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, template_folder='template')
+    app.config['SECRET_KEY'] = 'ncvbsh lkjbkjb'
+
+    from .view import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
+
+
